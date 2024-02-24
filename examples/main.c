baf2b99519368fe80d4b1990b56fd1b3dd401930
Tal Risin <risint96@gmail.com>
2024-02-24 15:11:51 +0200

    cleanup example

--- examples/main.c
+++ examples/main.c
@@ -4,17 +4,8 @@
 #include "cdefmt/include/cdefmt.h"
 
 int main(int argc, char* cargv[]) {
-  CDEFMT_INFO("HELLO\n");
-  CDEFMT_INFO("HELLO 2\n");
-  CDEFMT_INFO("HELLO 3\n");
-  CDEFMT_INFO("HELLO 3\n");
-  CDEFMT_VERBOSE("HELLO\n");
-  CDEFMT_DEBUG("HELLO 2\n");
-  CDEFMT_INFO("HELLO 2\n");
-  CDEFMT_WARNING("HELLO 2\n");
-  CDEFMT_ERROR("HELLO 2\n");
-  CDEFMT_ERROR("WHAT %u %u %u\n", 123);
-  CDEFMT_ERROR("WHAT %u %u %u %u %u %u %u %u %u %u\n", 1, 2, 3, 4, 5, 6, 7, 8);
+  CDEFMT_ERROR("WHAT {}", 123);
+  CDEFMT_ERROR("WHAT {0} {1} {2} {3} {4} {5} {6} {7}", 1, 2, 3, 4, 5, 6, 7, 8);
 }
 
 void cdefmt_log(const void* log, size_t size, enum cdefmt_level level) {
