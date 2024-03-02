4dfe983b4c06d339dcd8a884dd3aab1759dc3acc
Tal Risin <risint96@gmail.com>
2024-03-02 17:28:54 +0200

    update example with all possibilities I could think of

--- c/examples/stdout/main.c
+++ c/examples/stdout/main.c
@@ -5,48 +5,133 @@
 
 #include "cdefmt/include/cdefmt.h"
 
-struct some_struct {
-  uint32_t a;
-  uint16_t b;
+typedef struct some_struct {
+  uint64_t a;
+  uint32_t b;
+  uint16_t c;
+} some_struct_t;
+
+typedef struct some_padded_struct {
+  uint16_t a;
+  uint32_t b;
   uint64_t c;
-};
+} some_padded_struct_t;
 
-struct empty_struct {};
+typedef struct __attribute__((packed)) some_packed_struct {
+  uint16_t a;
+  uint32_t b;
+  uint64_t c;
+} some_packed_struct_t;
 
-enum some_enum {
-  SOME_ENUM_FIRST,
-  SOME_ENUM_WHAAAAT = 54,
-  SOME_ENUM_LOLZ,
-  SOME_ENUM_LAST = UINT64_MAX,
-};
+typedef enum unsigned_enum {
+  UNSIGNED_ENUM_1,
+  UNSIGNED_ENUM_2,
+  UNSIGNED_ENUM_3,
+  UNSIGNED_ENUM_4,
+  UNSIGNED_ENUM_5,
+} unsigned_enum_t;
 
-enum some_signed_enum {
-  SOME_SIGNED_ENUM_1 = 0,
-  SOME_SIGNED_ENUM_2 = INT32_MAX,
-  SOME_SIGNED_ENUM_3 = INT32_MIN,
-};
+typedef enum signed_enum {
+  SIGNED_ENUM_1 = 3,
+  SIGNED_ENUM_2 = 2,
+  SIGNED_ENUM_3 = 0,
+  SIGNED_ENUM_4 = -1,
+  SIGNED_ENUM_5 = INT16_MIN,
+} signed_enum_t;
 
 int main(int argc, char* cargv[]) {
-  uint32_t a = 5;
+  // Different log levels:
+  // CDEFMT_ERROR("This is an error log.");
+  // CDEFMT_WARNING("This is a warning log.");
+  // CDEFMT_INFO("This is an info log.");
+  // CDEFMT_DEBUG("This is a debug log.");
+  // CDEFMT_VERBOSE("This is a verbose log.");
 
-  struct some_struct b = {
-      .a = 123,
-      .b = 543,
-      .c = 9123,
+  // Different arguments
+  bool some_bool = true;
+  uint8_t some_u8 = 123;
+  uint16_t some_u16 = 12345;
+  uint32_t some_u32 = 1234567890;
+  uint64_t some_u64 = 1234567890123456789;
+  int8_t some_i8 = -123;
+  int16_t some_i16 = -12345;
+  int32_t some_i32 = -1234567890;
+  int64_t some_i64 = -1234567890123456789;
+  float some_f32 = 123.4567890f;
+  double some_f64 = 123.4567890;
+  some_struct_t some_struct_typedefd = {
+      .a = 1234567890123456789,
+      .b = 1234567890,
+      .c = 12345,
   };
+  struct some_struct some_struct = {
+      .a = 1234567890123456789,
+      .b = 1234567890,
+      .c = 12345,
+  };
+  some_padded_struct_t some_padded_struct_typedefd = {
+      .a = 12345,
+      .b = 1234567890,
+      .c = 1234567890123456789,
+  };
+  struct some_padded_struct some_padded_struct = {
+      .a = 12345,
+      .b = 1234567890,
+      .c = 1234567890123456789,
+  };
+  some_packed_struct_t some_packed_struct_typedefd = {
+      .a = 12345,
+      .b = 1234567890,
+      .c = 1234567890123456789,
+  };
+  struct some_packed_struct some_packed_struct = {
+      .a = 12345,
+      .b = 1234567890,
+      .c = 1234567890123456789,
+  };
+  enum unsigned_enum some_unsigned_enum = UNSIGNED_ENUM_5;
+  enum signed_enum some_signed_enum = SIGNED_ENUM_5;
+  enum unsigned_enum other_unsigned_enum = UNSIGNED_ENUM_3;
+  enum signed_enum other_signed_enum = SIGNED_ENUM_3;
 
-  bool c = false;
+  CDEFMT_INFO("bool: [{}]", some_bool);
+  CDEFMT_INFO("u8:   [{}]", some_u8);
+  CDEFMT_INFO("u16:  [{}]", some_u16);
+  CDEFMT_INFO("u32:  [{}]", some_u32);
+  CDEFMT_INFO("u64:  [{}]", some_u64);
+  CDEFMT_INFO("i8:   [{}]", some_i8);
+  CDEFMT_INFO("i16:  [{}]", some_i16);
+  CDEFMT_INFO("i32:  [{}]", some_i32);
+  CDEFMT_INFO("i64:  [{}]", some_i64);
+  CDEFMT_INFO("f32:  [{}]", some_f32);
+  CDEFMT_INFO("f64:  [{}]", some_f64);
+  CDEFMT_INFO("some struct typedef'd:        {}", some_struct_typedefd);
+  CDEFMT_INFO("some struct:                  {}", some_struct);
+  CDEFMT_INFO("some padded struct typedef'd: {}", some_padded_struct_typedefd);
+  CDEFMT_INFO("some padded struct:           {}", some_padded_struct);
+  CDEFMT_INFO("some packed struct typedef'd: {}", some_packed_struct_typedefd);
+  CDEFMT_INFO("some packed struct:           {}", some_packed_struct);
+  CDEFMT_INFO("some unsigned enum:        [{}]", some_unsigned_enum);
+  CDEFMT_INFO("other unsigned enum:       [{}]", other_unsigned_enum);
+  CDEFMT_INFO("some unsigned enum direct: [{}]", UNSIGNED_ENUM_5);
+  CDEFMT_INFO("some signed enum:          [{}]", some_signed_enum);
+  CDEFMT_INFO("other signed enum:         [{}]", other_signed_enum);
+  CDEFMT_INFO("some signed enum direct:   [{}]", SIGNED_ENUM_5);
 
-  struct empty_struct d = {};
+  // Up to 8 arguments
+  CDEFMT_INFO("no args []");
+  CDEFMT_INFO("1 arg:  [{}]", 1);
+  CDEFMT_INFO("2 args: [{}, {}]", 1, 2);
+  CDEFMT_INFO("3 args: [{}, {}, {}]", 1, 2, 3);
+  CDEFMT_INFO("4 args: [{}, {}, {}, {}]", 1, 2, 3, 4);
+  CDEFMT_INFO("5 args: [{}, {}, {}, {}, {}]", 1, 2, 3, 4, 5);
+  CDEFMT_INFO("6 args: [{}, {}, {}, {}, {}, {}]", 1, 2, 3, 4, 5, 6);
+  CDEFMT_INFO("7 args: [{}, {}, {}, {}, {}, {}, {}]", 1, 2, 3, 4, 5, 6, 7);
+  CDEFMT_INFO("8 args: [{}, {}, {}, {}, {}, {}, {}, {}]", 1, 2, 3, 4, 5, 6, 7, 8);
 
-  enum some_enum e = SOME_ENUM_LOLZ;
-  enum some_signed_enum e2 = SOME_SIGNED_ENUM_2;
-
-  CDEFMT_ERROR("What just happened?! [{}] [{}]", a, e2);
-  CDEFMT_WARNING("This wasn't supposed to happen... [{}]", 123);
-  CDEFMT_INFO("Just letting you know: {} {}", true, d);
-  CDEFMT_DEBUG("Oh so you like debugging: {} [{}]", c, 123);
-  CDEFMT_VERBOSE("I love spam! {} {} {} {}", a, b, c, e);
+  // Handle user error
+  CDEFMT_INFO("HAHA I LIED! gave you no args at all! [{}, {}, {}, {}]");
+  CDEFMT_INFO("HAHA I LIED! gave you less args than in format string! [{}, {}, {}, {}]", 1, 2, 3);
 
   return 0;
 }
