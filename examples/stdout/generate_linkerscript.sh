3fcd33b89b44ff1ae160a4f32f2e45b83a87deeb
Tal Risin <risint96@gmail.com>
2024-03-06 19:56:37 +0200

    add default linkerscript, dumped from ld

--- examples/stdout/generate_linkerscript.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Get default linker, trim comments, cleanup empty lines at end of file, insert custom section
-$1 --verbose |
-    awk -- '/^===/ { show = 1 - show ; next } { if (show) print }' |
-    sed -e :a -e '/^\n*$/{$d;N;ba' -e '}' |
-    sed "\$i .cdefmt 0 (INFO) : { KEEP(*(.cdefmt .cdefmt.*)); }" \
-        >$2
